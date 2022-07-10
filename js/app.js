a25a6e9b48e97e85a1781de311b57ba3dfb38293
bessobrien <obrien.bess@gmail.com>
2022-07-10 12:51:12 -0600

    add js file with button call

--- js/app.js
+++ js/app.js
@@ -24,3 +24,28 @@ data.forEach((dataRow) => {
     );
 });
 }
+
+// add function to determine what to do after input
+function handleClick() {
+    
+    // grab dt value from filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+// check to see if date was entered and filter
+if (date) {
+    //apply filter to table to only keep rows where dt matches
+    filteredData = filteredData.filter(row => row.datetime === date);
+    
+}
+
+// rebuild the table using the filtered data
+// ifno date entered, then original table data
+buildTable(filteredData);
+}
+
+// attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// build the table when the page loads
+buildTable(tableData);
